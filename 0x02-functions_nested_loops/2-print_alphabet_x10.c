fa208b9a9ac852de4ca44f1a4cc773acdf62f803
Abdullahi Aminu <111045134+ambeeman@users.noreply.github.com>
2022-09-14 11:02:36 -0600

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,4 +1,5 @@
-#include "main.h"                                                                                                             
+#include "main.h"    
+
 /**                                                                                                                           
  * print_alphabet_x10 - function to print abc 10 times                                                                        
  *                                                                                                                            
@@ -12,7 +13,7 @@ void print_alphabet_x10(void)
         {                                                                                                                     
                 for (c = 'a'; c <= 'z'; c++)                                                                                          
                 {                                                                                                             
-                _putchar(c);                                                                                          
+                        _putchar(c);                                                                                          
                 }                                                                                                             
                 _putchar('\n');                                                                                               
         }                                                                                                                     
