9f8dee3f9f707d251b1e451d1d5a7ca3b30a097a
Abdullahi Aminu <111045134+ambeeman@users.noreply.github.com>
2022-09-26 12:07:03 -0600

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -7,7 +7,8 @@ include "main.h"
  * @n: number of bytes to be copied                                  
  *                                                                   
  * Return: pointer to the copied memory block                        
- */                                                                  
+ */    
+ 
 char *_memcpy(char *dest, char *src, unsigned int n)                 
 {                                                                    
         unsigned int i = 0;                                          
@@ -18,5 +19,4 @@ char *_memcpy(char *dest, char *src, unsigned int n)
         }                                                            
                                                                      
         return (dest);                                               
-}                                                                    
-^C                            
+}                  
