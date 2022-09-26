694501354c6deb55704fdfcfd58e8d110f830695
Abdullahi Aminu <111045134+ambeeman@users.noreply.github.com>
2022-09-26 12:08:03 -0600

    Update 1-memcpy.c

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,7 +11,7 @@ include "main.h"
  
 char *_memcpy(char *dest, char *src, unsigned int n)                 
 {                                                                    
-        unsigned int i = 0;                                          
+        unsigned int i = O;                                          
                                                                      
         for (; i < n; i++)                                           
         {                                                            
