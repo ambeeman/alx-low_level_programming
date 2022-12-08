238b750fba6dee677f44384a1adf7b1692e98cea
Abdullahi Aminu <abdoulalbpop1@gmail.com>
2022-12-08 08:28:14 -0800

    Quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+
+
+/**
+ * * main - prints exactly and that piece of art is useful
+ * 
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	write(aid that peice of art is useful\" -Dora, 2015-10-19\n", 59);
+	return (1);
+}
